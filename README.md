1f7449b35c4aef4698a63bfff8aab2054b2153f2
Naim <mail@thenaim.com>
2020-10-31 13:47:32 +0100

    added official Speedtest CLI

--- README.md
+++ README.md
@@ -1,25 +1,10 @@
-# SpeedTest
-Test internet bandwidth speed using speedtest.net
+## SPEEDTEST® CLI
+#### Internet connection measurement for developers
+Speedtest CLI brings the trusted technology and global server network behind Speedtest to the command line. Built for software developers, system administrators and computer enthusiasts alike, Speedtest CLI is the first official Linux-native Speedtest application backed by Ookla®.
 
-# Download & Run (without installation)
+## Download & Run
 
-`wget https://raw.githubusercontent.com/tankibaj/speedtest/master/speedtest.sh && bash speedtest.sh`
-
-Once it ends, you can run it again to Speed Test again.
-
-`bash speedtest.sh`
-
-# Install and Run
-
-#### Download and make it executable
-
-`wget https://raw.githubusercontent.com/tankibaj/speedtest/master/speedtest_installer.sh`
-
-`chmod 755 speedtest_installer.sh`
-
-#### Install
-
-./speedtest_installer.sh -i
+`wget https://raw.githubusercontent.com/tankibaj/speedtest/master/speedtest.sh && sudo bash speedtest.sh`
 
 #### Now run serverinfo command from anywhere using any user account simply as:
 
--- speedtest.sh
+++ speedtest.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 #Tankibaj
 #https://github.com/tankibaj
-if [[ -e speedtest.py ]]; then
-	chmod a+rx speedtest.py
-	clear
-	echo "Starting speedtest please wait....."
-	echo ""
-	echo ""
-	./speedtest.py --share
-	echo ""
-	echo "Done..."
-	echo ""
-	exit
-
-else
-	clear
-	wget https://raw.githubusercontent.com/tankibaj/speedtest/master/speedtest.py
-	chmod a+rx speedtest.py
-	clear
-	echo "Starting speedtest please wait....."
-	echo ""
-	echo ""
-	./speedtest.py --share
-	echo ""
-	echo "Done..."
-	echo ""
-	exit
-fi
+sudo apt-get install gnupg1 apt-transport-https dirmngr
+export INSTALL_KEY=379CE192D401AB61
+# Ubuntu versions supported: xenial, bionic
+# Debian versions supported: jessie, stretch, buster
+export DEB_DISTRO=$(lsb_release -sc)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+# Other non-official binaries will conflict with Speedtest CLI
+# Example how to remove using apt-get
+# sudo apt-get remove speedtest-cli
+sudo apt-get install speedtest
